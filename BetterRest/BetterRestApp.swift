eaff64841436a8746087bde6c9c94dc016c0c2af
leomuko <kezzieleo@gmail.com>
2023-03-23 12:04:35 +0300

    Set up Project

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by mac on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
